9cc51d29f03846e5e784e4c1fe781fb9e31a0698
Sam Bumgardner <sambumgardner1@gmail.com>
2017-01-04 12:19:18 -0600

    Created VarSizedBox interface.
    Since any class that uses BoxCreator needs to provide a
    set of variables as parameters, I set up this interface to
    ensure that any box-using class has the proper set of
    variables it needs to use BoxCreator properly.

---
+++ source/boxes/VarSizedBox.hx
@@ -0,0 +1,19 @@
+package boxes;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+/**
+ * Interface for use by any class that uses a box created by BoxCreator.
+ * 
+ * Declares a set of variables needed to interact with the BoxCreator static functions.
+ * 
+ * @author Samuel Bumgardner
+ */
+interface VarSizedBox 
+{
+	public var boxWidth:Int;
+	public var boxHeight:Int;
+	
+	public var boxSpriteSheet:FlxGraphicAsset;
+	public var cornerSize:Int;
+	public var backgroundSize:Int;
+}
