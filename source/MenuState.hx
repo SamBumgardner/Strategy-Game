73088f1089132f95bae232e8f56227315d86f994
Sam Bumgardner <sambumgardner1@gmail.com>
2017-01-10 12:22:55 -0600

    Added new functionality to MenuState for testing purposes.
    Everything in here is very rough, and not meant to be any sort of
    representation of how the final game will be put together. It's
    just a place for me to throw together code to do some quick
    testing, and as a way for me to demo the most recent changes
    to the game.
    
    In this commit, menus and the Observer pattern are on display.
    
    To open the the first menu, press "Z" (the confirm key).
    To open the next menu, press "Z" again, etc.
    To close the current menu and return to controlling the MapCursor,
        press "X" (the cancel key).
    
    While controlling the map cursor, any of the action keys (z, x, c, s, d)
    can be pressed to cause some text to be written to the console describing
    what sort of event occurred.
    
    When menus are open, pressing "Z" or "X" will cause a similar message to
    be written to the console. Additionally, pressing "Z" will write a message to
    the console stating which menu option the player selected by pressing "Z".
    
    Again, everything here is very rough, and is not meant to be permanent in
    any way.

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,8 +1,10 @@
 package;
 
 import flixel.input.keyboard.FlxKey;
+import inputHandlers.ActionInputHandler;
 import inputHandlers.MoveInputHandler;
 import boxes.BoxCreator;
+import menus.BasicMenu;
 import cursors.MapCursor;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -10,10 +12,32 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import menus.MenuTemplate;
+import utilities.InputEvent;
+import utilities.EventTypes;
+import utilities.Observed;
+import utilities.Observer;
+import utilities.UpdatingEntity;
 
-class MenuState extends FlxState
+using utilities.EventExtender;
+
+/**
+ * Currently a test bed for new features. 
+ * The code below is not meant to be permanent, it's just there to test and show off the
+ * components of the game that I'm currently working on.
+ */
+
+class MenuState extends FlxState implements Observer
 {
 	private var mapCursor:MapCursor;
+	private var menu:BasicMenu;
+	private var menu2:BasicMenu;
+	private var menu3:BasicMenu;
+	private var menu4:BasicMenu;
+	
+	private var updateableObjects:Array<UpdatingEntity> = new Array<UpdatingEntity>();
+	private var currentlyUpdatingIndex:Int = 0;
+	
 	override public function create():Void
 	{
 		super.create();
@@ -28,7 +52,8 @@ class MenuState extends FlxState
 			}
 		}
 		
-		mapCursor = new MapCursor();
+		mapCursor = new MapCursor(0);
+		mapCursor.subject.addObserver(this);
 		add(mapCursor.getTotalFlxGroup());
 		
 		BoxCreator.setBoxType(AssetPaths.box_test__png, 15, 15);
@@ -39,13 +64,78 @@ class MenuState extends FlxState
 		secondBox.y = 200;
 		add(secondBox);
 		
-		MoveInputHandler.setMoveKeys(FlxKey.UP, FlxKey.DOWN, FlxKey.LEFT, FlxKey.RIGHT);
+		MoveInputHandler.init(FlxKey.UP, FlxKey.DOWN, FlxKey.LEFT, FlxKey.RIGHT);
+		ActionInputHandler.init([FlxKey.Z, FlxKey.X, FlxKey.C, FlxKey.S, FlxKey.D]);
+		
+		menu = new BasicMenu(50, 100, ["Unit", "Status", "Options", "Suspend", "End"], 1);
+		menu.subject.addObserver(this);
+		add(menu.totalFlxGrp);
+		
+		menu2 = new BasicMenu(200, 20, ["Item", "Trade", "Wait"], 2);
+		menu2.subject.addObserver(this);
+		add(menu2.totalFlxGrp);
+		
+		menu3 = new BasicMenu(200, 150, ["Equip", "Trade", "Discard", "This is a really long menu entry!"], 3);
+		menu3.subject.addObserver(this);
+		add(menu3.totalFlxGrp);
+		
+		menu4 = new BasicMenu(400, 30, ["Smile", "Jump", "Wear Hat", "Nose", "Clap", "PSI Rockin'", "Stomp"], 4);
+		menu4.subject.addObserver(this);
+		add(menu4.totalFlxGrp);
+		
+		updateableObjects.push(mapCursor);
+		updateableObjects.push(menu);
+		updateableObjects.push(menu2);
+		updateableObjects.push(menu3);
+		updateableObjects.push(menu4);
+		
+		updateableObjects[currentlyUpdatingIndex].active = true;
+	}
+	
+	public function onNotify(event:InputEvent, notifier:Observed)
+	{
+		trace("Recieved an event with id", event.getID(), "and type", event.getType());
+		
+		if (event.getType() == EventTypes.CONFIRM)
+		{
+			if (Std.is(notifier, MenuTemplate))
+			{
+				trace("The selected MenuOption was: " + (cast notifier).currMenuOption.label.text);
+			}
+			
+			updateableObjects[currentlyUpdatingIndex].deactivate();
+			(cast updateableObjects[currentlyUpdatingIndex]).hide();
+			currentlyUpdatingIndex++;
+			
+			if (currentlyUpdatingIndex == updateableObjects.length)
+			{
+				currentlyUpdatingIndex = 0;
+			}
+			
+			updateableObjects[currentlyUpdatingIndex].activate();
+			(cast updateableObjects[currentlyUpdatingIndex]).reveal();
+			MoveInputHandler.resetNumVars();
+			ActionInputHandler.resetNumVars();
+		}
+		else if (event.getType() == EventTypes.CANCEL)
+		{
+			updateableObjects[currentlyUpdatingIndex].deactivate();
+			(cast updateableObjects[currentlyUpdatingIndex]).hide();
+			
+			currentlyUpdatingIndex = 0;
+			
+			updateableObjects[currentlyUpdatingIndex].activate();
+			(cast updateableObjects[currentlyUpdatingIndex]).reveal();
+			MoveInputHandler.resetNumVars();
+			ActionInputHandler.resetNumVars();
+		}
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		mapCursor.update(elapsed);
+		updateableObjects[currentlyUpdatingIndex].update(elapsed);
 		MoveInputHandler.updateCycleFinished();
+		ActionInputHandler.updateCycleFinished();
 	}
 }
