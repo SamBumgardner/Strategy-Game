916a03ebfd919f275f55c3b4d291a1be8354344c
Sam Bumgardner <sambumgardner1@gmail.com>
2017-01-11 10:24:35 -0600

    Created MissionState & set as default game state.
    At this point, MissionState is basically the same as MenuState. Making a
    new state was necessary, however, because this branch involves setting
    up a camera and a map, both of which are going to be final elements
    in the mission sections of the game. Rather than do a bunch of work
    in a test environment and try to move it to a new state later, I just set
    up the MissionState now as the target for development related to
    the map and camera.

---
+++ source/missions/MissionState.hx
@@ -0,0 +1,169 @@
+package missions;
+
+import cursors.MapCursor;
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.group.FlxGroup;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxRect;
+import flixel.tile.FlxTilemap;
+import inputHandlers.ActionInputHandler;
+import inputHandlers.MoveInputHandler;
+import menus.BasicMenu;
+import menus.MenuTemplate;
+import observerPattern.Observed;
+import observerPattern.Observer;
+import observerPattern.eventSystem.EventTypes;
+import observerPattern.eventSystem.InputEvent;
+import utilities.UpdatingEntity;
+
+using observerPattern.eventSystem.EventExtender;
+
+/**
+ * ...
+ * @author Samuel Bumgardner
+ */
+class MissionState extends FlxState implements Observer
+{
+	
+	private var map:FlxOgmoLoader;
+	private var terrain:FlxTilemap;
+	
+	private var mapCursor:MapCursor;
+	
+	private var tileSize(default, never):Int = 64;
+	
+	private var menu:BasicMenu;
+	private var menu2:BasicMenu;
+	private var menu3:BasicMenu;
+	private var menu4:BasicMenu;
+	
+	private var updateableObjects:Array<UpdatingEntity> = new Array<UpdatingEntity>();
+	private var currentlyUpdatingIndex:Int = 0;
+
+	
+	override public function create():Void
+	{	
+		MoveInputHandler.init(FlxKey.UP, FlxKey.DOWN, FlxKey.LEFT, FlxKey.RIGHT);
+		ActionInputHandler.init([FlxKey.Z, FlxKey.X, FlxKey.C, FlxKey.S, FlxKey.D]);
+		
+		initMap();
+		initMapCursor();
+		
+		
+		
+		
+		
+		menu = new BasicMenu(50, 100, ["Unit", "Status", "Options", "Suspend", "End"], 1);
+		menu.subject.addObserver(this);
+		add(menu.totalFlxGrp);
+		
+		menu2 = new BasicMenu(200, 20, ["Item", "Trade", "Wait"], 2);
+		menu2.subject.addObserver(this);
+		add(menu2.totalFlxGrp);
+		
+		menu3 = new BasicMenu(200, 150, ["Equip", "Trade", "Discard", "This is a really long menu entry!"], 3);
+		menu3.subject.addObserver(this);
+		add(menu3.totalFlxGrp);
+		
+		menu4 = new BasicMenu(400, 30, ["Smile", "Jump", "Wear Hat", "Nose", "Clap", "PSI Rockin'", "Stomp"], 4);
+		menu4.subject.addObserver(this);
+		add(menu4.totalFlxGrp);
+		
+		updateableObjects.push(menu);
+		updateableObjects.push(menu2);
+		updateableObjects.push(menu3);
+		updateableObjects.push(menu4);
+		
+		updateableObjects[currentlyUpdatingIndex].active = true;
+		
+		
+		super.create();
+	}
+	
+	private function initMap():Void
+	{
+		map = new FlxOgmoLoader(AssetPaths.forest_1__oel);
+		terrain = map.loadTilemap(AssetPaths.terrain_forest__png, 64, 64, "terrain");
+		terrain.follow();
+		add(terrain);
+		
+		map.loadEntities(placeEntitites, "entities");
+	}
+	
+	/**
+	 * MapCursor should have already been created by initMap's placeEntities
+	 */
+	private function initMapCursor():Void
+	{
+		add(mapCursor.totalFlxGrp);
+		updateableObjects.push(mapCursor);
+	}
+	
+	private function placeEntitites(entityName:String, entityData:Xml):Void
+	{
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		if (entityName == "map_cursor")
+		{
+			mapCursor = new MapCursor(0);
+			mapCursor.subject.addObserver(this);
+			
+			var row:Int = Math.floor(y / tileSize);
+			var col:Int = Math.floor(x / tileSize);
+			mapCursor.jumpToPosition(row, col);
+		}
+	}
+	
+	
+	/* INTERFACE observerPattern.Observer */
+	
+	public function onNotify(event:InputEvent, notifier:Observed)
+	{
+		trace("Recieved an event with id", event.getID(), "and type", event.getType());
+		
+		if (event.getType() == EventTypes.CONFIRM)
+		{
+			if (Std.is(notifier, MenuTemplate))
+			{
+				trace("The selected MenuOption was: " + (cast notifier).currMenuOption.label.text);
+			}
+			
+			updateableObjects[currentlyUpdatingIndex].deactivate();
+			(cast updateableObjects[currentlyUpdatingIndex]).hide();
+			currentlyUpdatingIndex++;
+			
+			if (currentlyUpdatingIndex == updateableObjects.length)
+			{
+				currentlyUpdatingIndex = 0;
+			}
+			
+			updateableObjects[currentlyUpdatingIndex].activate();
+			(cast updateableObjects[currentlyUpdatingIndex]).reveal();
+			MoveInputHandler.resetNumVars();
+			ActionInputHandler.resetNumVars();
+		}
+		else if (event.getType() == EventTypes.CANCEL)
+		{
+			updateableObjects[currentlyUpdatingIndex].deactivate();
+			(cast updateableObjects[currentlyUpdatingIndex]).hide();
+			
+			currentlyUpdatingIndex = 0;
+			
+			updateableObjects[currentlyUpdatingIndex].activate();
+			(cast updateableObjects[currentlyUpdatingIndex]).reveal();
+			MoveInputHandler.resetNumVars();
+			ActionInputHandler.resetNumVars();
+		}
+	}
+	
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		updateableObjects[currentlyUpdatingIndex].update(elapsed);
+		MoveInputHandler.updateCycleFinished();
+		ActionInputHandler.updateCycleFinished();
+	}
+	
+}
