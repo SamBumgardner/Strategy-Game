44add6db92ae1348cf2fee7cc006eb49a63213c6
Sam Bumgardner <sambumgardner1@gmail.com>
2017-01-30 00:07:13 -0600

    Created menu-related enums.
    Includes one for all types of menus needed in a mission and
    three for the specific options for three different menus.
    
    These will be used to set up IDs for menus and MenuOptions,
    and be used to index arrays that are ordered to match these enums.

---
+++ source/menus/optionEnums/ItemActionMenuOptions.hx
@@ -0,0 +1,22 @@
+package menus.optionEnums;
+
+/**
+ * Enum for the different ways a unit can interact with items in its inventory.
+ * 
+ * IMPORTANT NOTE:
+ * 	If you change or add any entries to this enum, you also need to change the 
+ * 	array of strings used in MissionMenuCreator's makeItemActionMenu() to match, since 
+ * 	this enum's entries are expected to match the order (and the IDs) assigned to 
+ * 	each MenuOption in that menu.
+ * 
+ * @author Samuel Bumgardner
+ */
+
+@:enum
+class ItemActionMenuOptions
+{
+	public static var EQUIP(default, never)			= 0;
+	public static var USE(default, never)			= 1; 
+	public static var TRADE(default, never)			= 2;
+	public static var DISCARD(default, never)		= 3;
+}
