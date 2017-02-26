6aa4f450b96dc45c33494ff2857940fb9679e303
Sam Bumgardner <sambumgardner1@gmail.com>
2017-02-26 16:21:12 -0600

    Created TileChange class.
    TileChange objects are used to track changes that have happened to the map as a result of
    units moving. A unit cannot move through a hostile unit, so when a hostile unit takes a new position
    its old spot needs to be noted as newly opened, and its new spot needs to be noted as newly closed.
    This class has all of the variables needed to describe these changes fully.
    
    Will be used in the upcoming UnitManager class.

---
+++ source/units/TileChange.hx
@@ -0,0 +1,57 @@
+package units;
+import units.Unit.TeamID;
+
+/**
+ * Small class to wrap up data about a change to a particular tile of the map. 
+ * Indicates a tile location, whether that tile was opened or closed, and the team 
+ * 	affiliation of the opening/closing event.
+ * 
+ * Used in UnitManager to track the changes that a unit should consider when recalculating
+ * 	its movement range.
+ * 
+ * @author Samuel Bumgardner
+ */
+class TileChange
+{
+	/**
+	 * Indicates the location on the map that this TileChange references.
+	 */
+	public var moveID(default, null):MoveID;
+	
+	/**
+	 * If true, then the change resulted in the tile opening.
+	 * If false, then the change resulted in the tile closing.
+	 */
+	public var wasOpened(default, null):Bool;
+	
+	/**
+	 * The teamID of the unit that caused the change.
+	 * Units can ignore changes made by their own team.
+	 * Changes by the "NONE" team must be observed by all units.
+	 */
+	public var causedBy(default, null):TeamID;
+	
+	/**
+	 * Array of integers that indicate which unitIDs have processed TileChanges up to
+	 * 	and including this tile, but no farther. Used in UnitManager when attempting to
+	 * 	remove TileChanges that have been processed by all units, and thus no longer need
+	 * 	to be remembered.
+	 */
+	public var unitsHaveProcessed:Array<Int>;
+	
+	/**
+	 * Initializer.
+	 * 
+	 * @param	tileLoc	MoveID of the tile that was changed.
+	 * @param	opened	True if tile was opened, false if closed.
+	 * @param	team	teamID of the unit that caused this move.
+	 */
+	public function new(tileLoc:MoveID, opened:Bool, team:TeamID) 
+	{
+		moveID = tileLoc;
+		wasOpened = opened;
+		causedBy = team;
+		unitsHaveProcessed = new Array<Int>();
+	}
+	
+}
