804cd64c813aaa819ab44efb25fcd936d5b3bce1
Sam Bumgardner <sambumgardner1@gmail.com>
2017-03-07 23:31:22 -0600

    Created Item class.
    More specific items and item types will end up inheriting from this one.
    For now, I'm focusing on including variables and functions that will be used
    with inventory management.

---
+++ source/units/items/Item.hx
@@ -0,0 +1,26 @@
+package units.items;
+import units.Unit;
+
+/**
+ * ...
+ * @author Samuel Bumgardner
+ */
+class Item
+{
+
+	public var durability:Int;
+	public var inventory:Inventory;
+	public var invIndex:Int;
+	public var owner:Unit;
+	
+	public function new() 
+	{
+		
+	}
+	
+	public function use(usingUnit:Unit, targetUnit:Unit)
+	{
+		
+	}
+	
+}
