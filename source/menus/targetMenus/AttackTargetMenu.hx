2ee538208a1fd99ad53c25dad91d24809a43a6c3
Sam Bumgardner <SamBumgardner1@gmail.com>
2017-04-06 15:37:21 -0500

    Added documentation to AttackTargetMenu, and adjusted to use new unit-weapon logic.
    Since the Unit now has the equippedItem property (of the Item type) rather than equippedWeapon,
    the AttackTargetMenu now needs to use equippedItem and cast it to the Weapon type.

--- source/menus/targetMenus/AttackTargetMenu.hx
+++ source/menus/targetMenus/AttackTargetMenu.hx
@@ -13,12 +13,21 @@ class AttackTargetMenu extends TargetMenuTemplate
 {
 	public var selectedUnit:Unit;
 	
+	/**
+	 * Identifies all valid targets within this unit's equipped item's range, then
+	 * 	stores those units as an array of OnMapEntity objects in its possibleTargets array.
+	 * 
+	 * Assumes that the selected unit has a WeaponItem-type item equipped. 
+	 * If not, unsafe casting will result in a crash.
+	 * 
+	 * @param	parentState
+	 */
 	public override function refreshTargets(parentState:MissionState):Void
 	{
 		selectedUnit = parentState.getSelectedUnit();
 		
 		// Get array of valid units, then cast to store as Array<OnMapEntity>
 		possibleTargets = cast parentState.getValidUnitsInRange(
-			selectedUnit.equippedWeapon.attackRanges, SimpleTargetTests.enemyUnitTest);
+			(cast selectedUnit.equippedItem).attackRanges, SimpleTargetTests.enemyUnitTest);
 	}
 }
