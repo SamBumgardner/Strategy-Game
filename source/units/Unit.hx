a16cec6e3541c13a09f85ac36c8e68c52edf1b38
Sam Bumgardner <sambumgardner1@gmail.com>
2017-04-19 00:38:36 -0500

    Removed old debug code for testing unit animations.

--- source/units/Unit.hx
+++ source/units/Unit.hx
@@ -341,15 +341,6 @@ class Unit extends FlxSprite implements Observed implements OnMapEntity
 	override public function update(elapsed:Float):Void 
 	{
 		super.update(elapsed);
-		
-		if (FlxG.keys.justPressed.A)
-		{
-			animation.play("idle");
-		}
-		if (FlxG.keys.justPressed.S)
-		{
-			animation.play("hover");
-		}
 	}
 	
 }
