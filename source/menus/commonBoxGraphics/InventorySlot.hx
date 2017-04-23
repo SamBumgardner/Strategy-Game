61cf3678353ba7fa0100ea6a78afaecb2bcdb0cf
Sam Bumgardner <sambumgardner1@gmail.com>
2017-04-22 23:53:44 -0500

    Created InventorySlot class.
    Multiple menus may need inventory slots in them, so I put together a single class
    that all can use when an inventory slot is needed somewhere. This should cut
    down on calls to BoxCreator's createBox() function, which is good.

---
+++ source/menus/commonBoxGraphics/InventorySlot.hx
@@ -0,0 +1,41 @@
+package menus.commonBoxGraphics;
+
+import boxes.BoxCreator;
+import boxes.VarSizedBox;
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+/**
+ * ...
+ * @author Samuel Bumgardner
+ */
+class InventorySlot extends FlxSprite implements VarSizedBox
+{
+
+	/**
+	 * Variables to satisfy VarSizedBox interface.
+	 * Specifies the qualities of the menu's box.
+	 */
+	public var cornerSize(default, never):Int		= 24;
+	public var backgroundSize(default, never):Int	= 24;
+	public var boxWidth(default, null):Int = 300 - 32;
+	public var boxHeight(default, null):Int = 48;
+	public var boxSpriteSheet(default, null):FlxGraphicAsset = AssetPaths.inv_slot__png;
+	
+	public static var sharedGraphic:FlxSprite;
+	
+	public function new(?X:Float=0, ?Y:Float=0) 
+	{
+		super(X, Y);
+		
+		if (sharedGraphic == null)
+		{
+			BoxCreator.setBoxType(boxSpriteSheet, cornerSize, backgroundSize);
+			sharedGraphic = BoxCreator.createBox(boxWidth, boxHeight);
+		}
+		
+		loadGraphicFromSprite(sharedGraphic);
+		active = false;
+	}
+	
+}
