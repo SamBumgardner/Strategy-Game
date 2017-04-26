5ee2082acc4a2c9dae8e574f64c6f0bfd9c2454f
Sam Bumgardner <SamBumgardner1@gmail.com>
2017-04-26 12:08:17 -0500

    Began moving functionality from InventoryMenu to InventoryBox.
    Since multiple menus/things want to display an inventory menu with the same capabilities,
    I figured it'd be better to move the box and menuOption creation into a separate class, and
    then compose it inside menus that want to show/use an inventory.

---
+++ source/menus/cursorMenus/InventoryBox.hx
@@ -0,0 +1,201 @@
+package menus.cursorMenus;
+
+import boxes.ResizableBox;
+import boxes.VarSizedBox;
+import flixel.group.FlxGroup;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import units.items.Inventory;
+
+/**
+ * ...
+ * @author Sam Bumgardner
+ */
+class InventoryBox implements VarSizedBox 
+{
+	///////////////////////////////////////
+	//         DATA  DECLARATION         //
+	///////////////////////////////////////
+	
+	/**
+	 * Maximum number of inventory slots this menu can display.
+	 */
+	public var maxInventorySlots(default, never):Int = 7;
+	
+	/**
+	 * Constant variables that define the font size of the item text and the vertical interval
+	 * 	between item boxes.
+	 */
+	private var textSize(default, never):Int = 16;
+	private var itemSlotInterval(default, never):Float = 40;
+	
+	
+	/**
+	 * Variables to satisfy VarSizedBox interface.
+	 * Specifies the qualities of the menu's box.
+	 */
+	public var cornerSize(default, never):Int		= 32;
+	public var backgroundSize(default, never):Int	= 32;
+	public var boxWidth(default, null):Int;
+	public var boxHeight(default, null):Int;
+	public var boxSpriteSheet(default, null):FlxGraphicAsset = AssetPaths.box_menu__png;
+	
+	/**
+	 * Object that contains a set of sprites that can be used as a resizable box.
+	 * Call this object's public functions to do any resizing or interactions with
+	 *  the box.
+	 */
+	public var resizableBox:ResizableBox;
+	
+	
+	/**
+	 * Variable for keeping track of the menu's background box FlxSprite.
+	 */
+	public var boxSpriteGrp:FlxGroup;
+
+	/**
+	 * 
+	 */
+	public var itemSlotsGrp:FlxGroup;
+	
+	/**
+	 * 
+	 */
+	public var optionFlxGrp:FlxGroup = new FlxGroup();
+	
+	
+	/**
+	 * 
+	 */
+	public var totalFlxGrp:FlxGroup = new FlxGroup();
+	
+	/**
+	 * 
+	 */
+	public var trackedInventory(default, set):Inventory;
+	
+	
+	///////////////////////////////////////
+	//          INITIALIZATION           //
+	///////////////////////////////////////
+
+	public function new(?X:Float=0, ?Y:Float=0) 
+	{
+		initMenuBox(X, Y);
+		initInventorySlots(X, Y);
+		addAllFlxGrps();
+	}
+	
+	/**
+	 * Creates variable sized box sprite that sits behind inventory menu options.
+	 * 
+	 * Is also responsible for creating and populating the boxSpriteGrp FlxGroup.
+	 * 
+	 * @param	X				The desired x position of the menu.
+	 * @param	Y				The desired y position of the menu.
+	 * @param	maxTextWidth	The width of the largest MenuOption's label.
+	 */
+	private function initMenuBox(X:Float, Y:Float):Void
+	{
+		boxWidth = 300;
+		boxHeight = Math.floor(itemSlotInterval * maxInventorySlots + cornerSize);
+		
+		resizableBox = new ResizableBox(X, Y, boxWidth, boxHeight, boxSpriteSheet, 
+			cornerSize, backgroundSize);
+		
+		boxSpriteGrp = resizableBox.totalFlxGrp;
+	}
+	
+	private function initInventorySlots(X:Float, Y:Float):Void
+	{
+		var newMenuOption:MenuOption;
+		var menuOptionWidth:Float = boxWidth - cornerSize * 2;
+		
+		itemSlotsGrp = new FlxGroup();
+		
+		for (i in 0...maxInventorySlots)
+		{
+			var slotX:Float = X + cornerSize / 2;
+			var slotY:Float = Y + cornerSize / 2 + itemSlotInterval * i;
+			var optionOffsetY:Float = 9;
+			
+			itemSlotsGrp.add(new InventorySlot(slotX, slotY));
+			
+			newMenuOption = new MenuOption(slotX + cornerSize, slotY + optionOffsetY, i, 
+				menuOptionWidth, "", textSize);
+			newMenuOption.label.color = FlxColor.BLACK;
+			menuOptionArr.push(newMenuOption);
+			
+			// Connect the current menu option and the previous option as neighbors.
+			if (i > 0) 
+			{
+				newMenuOption.upOption = menuOptionArr[i - 1];
+				menuOptionArr[i - 1].downOption = newMenuOption;
+			}
+			
+			// Connect the last menu option to the first menu option as neighbors.
+			if (i == maxInventorySlots - 1)
+			{
+				menuOptionArr[0].upOption = menuOptionArr[i];
+				menuOptionArr[0].upIsWrap = true;
+				menuOptionArr[i].downOption = menuOptionArr[0];
+				menuOptionArr[i].downIsWrap = true;
+			}
+			
+			// Add the current menu option to the totalFlxGrp
+			optionFlxGrp.add(newMenuOption.totalFlxGrp);
+		}
+	}
+	
+	private function addAllFlxGrps():Void
+	{
+		totalFlxGrp.add(boxSpriteGrp);
+		totalFlxGrp.add(itemSlotsGrp);
+		totalFlxGrp.add(optionFlxGrp);
+	}
+	
+	
+	public function set_trackedInventory(newInv:Inventory)
+	{
+		trackedInventory = newInv;
+		refreshDisplay()
+	}
+	
+	public function refreshDisplay():Void
+	{
+		for (i in 0...maxInventorySlots)
+		{
+			if (i < selectedUnit.inventory.items.length)
+			{
+				menuOptionArr[i].label.text = selectedUnit.inventory.items[i].name;
+				
+				
+				// Connect the current menu option and the previous option as neighbors.
+				if (i > 0) 
+				{
+					menuOptionArr[i].upOption = menuOptionArr[i - 1];
+					menuOptionArr[i - 1].downOption = menuOptionArr[i];
+				}
+				
+				// Connect the last menu option to the first menu option as neighbors.
+				if (i == selectedUnit.inventory.items.length - 1)
+				{
+					menuOptionArr[0].upOption = menuOptionArr[i];
+					menuOptionArr[0].upIsWrap = true;
+					menuOptionArr[i].downOption = menuOptionArr[0];
+					menuOptionArr[i].downIsWrap = true;
+				}
+				
+				itemSlotsGrp.members[i].visible = true;
+				menuOptionArr[i].reveal();
+			}
+			else // Deactivate other item slots.
+			{
+				itemSlotsGrp.members[i].visible = false;
+				menuOptionArr[i].hide();
+			}
+		}
+		
+		boxHeight = Math.floor(itemSlotInterval * selectedUnit.inventory.items.length + cornerSize);
+		resizableBox.resize(boxWidth, boxHeight);
+	}
+}
--- source/menus/cursorMenus/InventoryMenu.hx
+++ source/menus/cursorMenus/InventoryMenu.hx
@@ -22,35 +22,9 @@ class InventoryMenu extends CursorMenuTemplate implements VarSizedBox
 	///////////////////////////////////////
 	
 	/**
-	 * Maximum number of inventory slots this menu can display.
+	 *
 	 */
-	public var maxInventorySlots(default, never):Int = 7;
-	
-	/**
-	 * Constant variables that define the font size of the item text and the vertical interval
-	 * 	between item boxes.
-	 */
-	private var textSize(default, never):Int = 16;
-	private var itemSlotInterval(default, never):Float = 40;
-	
-	
-	/**
-	 * Variables to satisfy VarSizedBox interface.
-	 * Specifies the qualities of the menu's box.
-	 */
-	public var cornerSize(default, never):Int		= 32;
-	public var backgroundSize(default, never):Int	= 32;
-	public var boxWidth(default, null):Int;
-	public var boxHeight(default, null):Int;
-	public var boxSpriteSheet(default, null):FlxGraphicAsset = AssetPaths.box_menu__png;
-	
-	/**
-	 * Object that contains a set of sprites that can be used as a resizable box.
-	 * Call this object's public functions to do any resizing or interactions with
-	 *  the box.
-	 */
-	public var resizableBox:ResizableBox;
-	
+	private var inventoryBox:InventoryBox;
 	
 	/**
 	 * Variable for keeping track of the menu's background box FlxSprite.
@@ -80,81 +54,25 @@ class InventoryMenu extends CursorMenuTemplate implements VarSizedBox
 	public function new(?X:Float=0, ?Y:Float=0, ?subjectID:Int=0) 
 	{
 		super(X, Y, subjectID);
-		initMenuBox(X, Y);
-		initInventorySlots(X, Y);
+		initInventoryBox(X, Y);
 		initBasicCursor();
 		addAllFlxGrps();
 		setScrollFactors();
-		trace(boxHeight);
 		hide();
 	}
 	
-	/**
-	 * Creates variable sized box sprite that sits behind inventory menu options.
-	 * 
-	 * Is also responsible for creating and populating the boxSpriteGrp FlxGroup.
-	 * 
-	 * @param	X				The desired x position of the menu.
-	 * @param	Y				The desired y position of the menu.
-	 * @param	maxTextWidth	The width of the largest MenuOption's label.
-	 */
-	private function initMenuBox(X:Float, Y:Float):Void
+	private function initInventoryBox(X:Float, Y:Float):Void
 	{
-		boxWidth = 300;
-		boxHeight = Math.floor(itemSlotInterval * maxInventorySlots + cornerSize);
+		inventoryBox = new InventoryBox(X, Y);
 		
-		resizableBox = new ResizableBox(X, Y, boxWidth, boxHeight, boxSpriteSheet, 
-			cornerSize, backgroundSize);
-		
-		boxSpriteGrp = resizableBox.totalFlxGrp;
 	}
 	
-	private function initInventorySlots(X:Float, Y:Float):Void
-	{
-		var newMenuOption:MenuOption;
-		var menuOptionWidth:Float = boxWidth - cornerSize * 2;
-		
-		itemSlotsGrp = new FlxGroup();
-		
-		for (i in 0...maxInventorySlots)
-		{
-			var slotX:Float = X + cornerSize / 2;
-			var slotY:Float = Y + cornerSize / 2 + itemSlotInterval * i;
-			var optionOffsetY:Float = 9;
-			
-			itemSlotsGrp.add(new InventorySlot(slotX, slotY));
-			
-			newMenuOption = new MenuOption(slotX + cornerSize, slotY + optionOffsetY, i, 
-				menuOptionWidth, "", textSize);
-			newMenuOption.label.color = FlxColor.BLACK;
-			menuOptionArr.push(newMenuOption);
-			
-			// Connect the current menu option and the previous option as neighbors.
-			if (i > 0) 
-			{
-				newMenuOption.upOption = menuOptionArr[i - 1];
-				menuOptionArr[i - 1].downOption = newMenuOption;
-			}
-			
-			// Connect the last menu option to the first menu option as neighbors.
-			if (i == maxInventorySlots - 1)
-			{
-				menuOptionArr[0].upOption = menuOptionArr[i];
-				menuOptionArr[0].upIsWrap = true;
-				menuOptionArr[i].downOption = menuOptionArr[0];
-				menuOptionArr[i].downIsWrap = true;
-			}
-			
-			// Add the current menu option to the totalFlxGrp
-			optionFlxGrp.add(newMenuOption.totalFlxGrp);
-		}
-	}
 	
 	override private function addAllFlxGrps():Void
 	{
-		totalFlxGrp.add(boxSpriteGrp);
-		totalFlxGrp.add(itemSlotsGrp);
-		totalFlxGrp.add(optionFlxGrp);
+		totalFlxGrp.add(inventoryBox.boxSpriteGrp);
+		totalFlxGrp.add(inventoryBox.itemSlotsGrp);
+		totalFlxGrp.add(inventoryBox.optionFlxGrp);
 		totalFlxGrp.add(menuCursor);
 	}
 	
@@ -177,42 +95,8 @@ class InventoryMenu extends CursorMenuTemplate implements VarSizedBox
 	public function refreshMenuOptions(parentState:MissionState):Void
 	{
 		selectedUnit = parentState.getSelectedUnit();
+		inventoryBox.trackedInventory = selectedUnit.inventory;
 		
-		for (i in 0...maxInventorySlots)
-		{
-			if (i < selectedUnit.inventory.items.length)
-			{
-				menuOptionArr[i].label.text = selectedUnit.inventory.items[i].name;
-				
-				
-				// Connect the current menu option and the previous option as neighbors.
-				if (i > 0) 
-				{
-					menuOptionArr[i].upOption = menuOptionArr[i - 1];
-					menuOptionArr[i - 1].downOption = menuOptionArr[i];
-				}
-				
-				// Connect the last menu option to the first menu option as neighbors.
-				if (i == selectedUnit.inventory.items.length - 1)
-				{
-					menuOptionArr[0].upOption = menuOptionArr[i];
-					menuOptionArr[0].upIsWrap = true;
-					menuOptionArr[i].downOption = menuOptionArr[0];
-					menuOptionArr[i].downIsWrap = true;
-				}
-				
-				itemSlotsGrp.members[i].visible = true;
-				menuOptionArr[i].reveal();
-			}
-			else // Deactivate other item slots.
-			{
-				itemSlotsGrp.members[i].visible = false;
-				menuOptionArr[i].hide();
-			}
-		}
-		
-		boxHeight = Math.floor(itemSlotInterval * selectedUnit.inventory.items.length + cornerSize);
-		resizableBox.resize(boxWidth, boxHeight);
 	}
 	
 	override private function moveResponse(vertMove:Int, horizMove:Int, heldMove:Bool):Void
