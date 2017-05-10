b6c3db2221396736320b6d3e079f6721371ec608
Sam Bumgardner <sambumgardner1@gmail.com>
2017-05-09 19:37:15 -0500

    Added combatHealthBox instantiation to CombatMenu.

--- source/menus/CombatMenu.hx
+++ source/menus/CombatMenu.hx
@@ -3,6 +3,7 @@ package menus;
 import boxes.VarSizedBox;
 import flixel.FlxG;
 import flixel.system.FlxAssets.FlxGraphicAsset;
+import menus.commonBoxGraphics.CombatHealthBox;
 import missions.MissionState;
 import units.Unit;
 
@@ -10,38 +11,36 @@ import units.Unit;
  * ...
  * @author Samuel Bumgardner
  */
-class CombatMenu extends MenuTemplate implements VarSizedBox
+class CombatMenu extends MenuTemplate 
 {
-
-	/* INTERFACE boxes.VarSizedBox */
 	
-	public var boxWidth(default, null):Int;
-	
-	public var boxHeight(default, null):Int;
-	
-	public var boxSpriteSheet(default, null):FlxGraphicAsset;
-	
-	public var cornerSize(default, null):Int;
-	
-	public var backgroundSize(default, null):Int;
-	
+	public var combatHealthBox1:CombatHealthBox;
+	public var combatHealthBox2:CombatHealthBox;
 	
 	public function new(?X:Float=0, ?Y:Float=0, ?subjectID:Int=0) 
 	{
 		super(X, Y, subjectID);
 		
 		// Create a pair of CombatHealthBox objects
-		// Position itself appropriately.
+		combatHealthBox1 = new CombatHealthBox(X, Y);
+		combatHealthBox2 = new CombatHealthBox(combatHealthBox1.boxSprite.x + 
+			combatHealthBox1.boxWidth, Y);
+		
+		totalFlxGrp.add(combatHealthBox1.totalFlxGroup);
+		totalFlxGrp.add(combatHealthBox2.totalFlxGroup);
+		
+		hide();
 	}
 	
 	public function setUnits(attacker:Unit, defender:Unit):Void
 	{
+		combatHealthBox1.setUnit(attacker);
+		combatHealthBox2.setUnit(defender);
 		// Set up combat variables, get numeric versions of needed derived statistics.
 	}
 	
 	public function beginCombat():Void
 	{
-		
 		// Start a series of timed events. Initiator attacks, defender counterattacks, etc.
 	}
 	
@@ -49,6 +48,7 @@ class CombatMenu extends MenuTemplate implements VarSizedBox
 	{
 		// I think this one is the meat of the combat logic. 
 		// maybe. Might need some other name for it. Starts next attack, checks for death, etc.
+		
 	}
 	
 	/**
@@ -65,8 +65,11 @@ class CombatMenu extends MenuTemplate implements VarSizedBox
 	
 	// Call CombatHealthBox updates
 	// Decide if the next phase of combat is ready to begin.
-	public function update(elapsed:Float):Void
+	override public function update(elapsed:Float):Void
 	{
+		super.update(elapsed);
 		
+		combatHealthBox1.update(elapsed);
+		combatHealthBox2.update(elapsed);
 	}
 }
